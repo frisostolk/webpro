f8edd563d25ad6bbb63de02a807a88a6cc199f61
frisostolk <frisostolk98@gmail.com>
2019-05-29 22:30:04 +0200

    plaatjes toegevoegd, javascript werkt nog steeds niet

---
+++ game.php
@@ -0,0 +1,54 @@
+<?php
+?>
+<script>
+    const cardsArray = [
+        {
+            name: 'birdo',
+            img: '../data/img/birdo.png',
+        },
+        {
+            name: 'bob_de_bouwer',
+            img: '../data/img/bob_de_bouwer.png',
+        },
+        {
+            name: 'bowser',
+            img: '../data/img/bowser.png',
+        },
+        {
+            name: 'dry_bones',
+            img: '../data/img/dry_bones.png',
+        },
+        {
+            name: 'luigi',
+            img: 'img/luigi.png',
+        },
+        {
+            name: 'peach',
+            img: 'img/peach.png',
+        },
+        {
+            name: '1up',
+            img: 'img/1up.png',
+        },
+        {
+            name: 'mushroom',
+            img: 'img/mushroom.png',
+        },
+        {
+            name: 'thwomp',
+            img: 'img/thwomp.png',
+        },
+        {
+            name: 'bulletbill',
+            img: 'img/bulletbill.png',
+        },
+        {
+            name: 'coin',
+            img: 'img/coin.png',
+        },
+        {
+            name: 'goomba',
+            img: 'img/goomba.png',
+        },
+    ]
+</script>
--- tpl/welcome.php
+++ tpl/welcome.php
@@ -1,7 +1,7 @@
 <?php
     echo"<h2>Welcome Player!</h2>";
     ?>
-<form id="welcome_form">
+<form action="/webpro/game.php" method="POST" id="welcome_form">
     <div class="form-group row">
         <label for="email" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">
@@ -14,7 +14,49 @@
             <input type="text" onkeyup="check_name()" class="form-control" id="name" placeholder="name">
         </div>
     </div>
-    <div class="btn btn-primary mb-2">Let's play</div>
-    <script src="validate_form.js" type="text/javascript"></script>
+    <div id="submit" class="btn btn-primary mb-2">Let's play</div>
+    <script src="../scripts/validate_form.js" type="text/javascript"></script>
 
 </form>
+<script>function validateEmail(email) {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+    function check_email() {
+        let email = $('#email');
+        let email_value = email.val();
+        if (validateEmail(email_value)) {
+            email.removeClass("is-invalid");
+            email.addClass("is-valid");
+            return true
+        } else {
+            email.removeClass("is-valid");
+            email.addClass("is-invalid");
+            return false
+        }
+    }
+    function check_name() {
+        let name = $('#name');
+        let nameForm = name.val();
+        if (nameForm !== "") {
+            name.removeClass("is-invalid");
+            name.addClass("is-valid");
+            return true
+        } else if (nameForm === "") {
+            name.removeClass("is-valid");
+            name.addClass("is-invalid");
+            return false
+        }
+    }
+    function check_form() {
+
+        if(check_name && check_email ){
+            $("#welcome_form").submit();
+        }
+    }
+
+    window.onload = function() {
+        $( "#submit" ).click(function() {
+            check_form();
+        });
+    };</script>

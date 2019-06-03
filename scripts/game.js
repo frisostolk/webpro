2f924f99cb790a623e73e9e0c7b27bbcca095263
frisostolk <frisostolk98@gmail.com>
2019-06-03 15:30:04 +0200

    random 24 plaatjes gemaakt

--- scripts/game.js
+++ scripts/game.js
@@ -17,6 +17,38 @@ $(function() {
             name: 'dry_bones',
             img: 'data/img/dry_bones.png',
         },
+        {
+            name: 'yoshi',
+            img: 'data/img/yoshi.png',
+        },
+        {
+            name: 'mario',
+            img: 'data/img/mario.png',
+        },
+        {
+            name: 'spiderman',
+            img: 'data/img/spiderman.png',
+        },
+        {
+            name: 'koopa_troopa',
+            img: 'data/img/koopa_troopa.png',
+        },
+        {
+            name: 'luigi',
+            img: 'data/img/luigi.png',
+        },
+        {
+            name: 'wario',
+            img: 'data/img/wario.png',
+        },
+        {
+            name: 'iron_man',
+            img: 'data/img/iron_man.png',
+        },
+        {
+            name: 'captain_america',
+            img: 'data/img/captain.png',
+        },
     ];
 // For each item in the cardsArray array...
 
@@ -25,8 +57,10 @@ $(function() {
     let grid = document.createElement('section');
     grid.setAttribute('class', 'grid');
 // Append the grid section to the game div
+    let gameGrid = cardsArray.concat(cardsArray);
+    gameGrid.sort(() => 0.5 - Math.random())
     game.appendChild(grid);
-    cardsArray.forEach(item => {
+    gameGrid.forEach(item => {
         // Create a div
         const card = document.createElement('div');
 

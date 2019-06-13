a37d42381b16ebade17ed0031dea739987505f8d
brittbruntink <49716360+brittbruntink@users.noreply.github.com>
2019-06-13 22:43:50 +0200

    Deleted app.js

--- server/app.js
+++
@@ -1,9 +0,0 @@
-io.on('connection', (socket) => {
-    console.log('a user connected:', socket.id);
-    socket.on('disconnect', function() {
-        console.log('user disconnected');
-    });
-});
-const gameState = {
-    players: {}
-}

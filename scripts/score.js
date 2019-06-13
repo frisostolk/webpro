fe8d610997b313ce987dca461643883933053f3d
brittbruntink <brittbruntink@hotmail.com>
2019-06-13 16:49:59 +0200

    Added script for keeping score

--- scripts/score.js
+++ scripts/score.js
@@ -1,14 +1,5 @@
-let score1 = 0;
-let score2 = 0;
-let player1 = JSONIETSIP;
-let player2 = JSONIETSIP;
-
-if (player1 has match){
-    score1 = score1 + 1;
-}
-
-if (player2 has match) {
-    score2 = score2 + 1;
-}
-
-
+$.getJSON( "../webpro/players.json", function( players ){
+    var player1 = players[1].IP;
+    var player2 = players[2].IP;
+    alert(player2);
+});

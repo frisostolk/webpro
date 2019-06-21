7d01777731dc8cbfc92ee549a248fb23f84a424f
brittbruntink <49716360+brittbruntink@users.noreply.github.com>
2019-06-21 12:01:47 +0200

    updated generate_id.js

--- scripts/generate_id.js
+++ scripts/generate_id.js
@@ -1,7 +1,13 @@
-$(function generate_id() {
+$(function click_id(){
+    // Function that makes sure the function generate id is executed when the button is clicked.
+    document.getElementById("button").onclick = function() {generate_id()};
+    }
+);
+
+function generate_id() {
     // Math.random should be unique because of its seeding algorithm.
-    // Convert it to base 36 (numbers + letters), and grab the first 6 characters
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
     // after the decimal.
     var ID = Math.random().toString(36).substr(2, 6);
     document.getElementById("id").innerHTML = String(ID);
-});
+}

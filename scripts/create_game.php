92be7f5d9cf997e4e2442868461e2387a198d3f9
frisostolk <frisostolk98@gmail.com>
2019-06-24 14:07:01 +0200

    id.json

--- scripts/create_game.php
+++ scripts/create_game.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: frisostolk
- * Date: 2019-06-23
- * Time: 20:45
- */
+error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    $game_id = uniqid();
+    $files = scandir('../data/');
+    echo serialize($files);
+    if(in_array($game_id, $files)){
+        echo"sweet";
+    }
+    else{
+        session_start();
+            $game = array(
+                "sessionID0" => session_id(),
+                "sessionID1" => null,
+                "turn" => 0,
+                "state" => null,
+                "creationDateTime" => time(),
+                "lastActionDateTime" => time(),
+                "grid" => array(
+                )
+            );
+        $json_file = fopen($game_id.'.json', 'w');
+        fwrite($json_file, json_encode($game));
+    }
+?> Ben er nu weer ff was ff aan kijken naar sessions

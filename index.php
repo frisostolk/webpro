2ca1248c1abcf81ef90139fbbded260966c2a578
frisostolk <frisostolk98@gmail.com>
2019-06-24 11:56:40 +0200

    id.json wordt gemaakt

--- index.php
+++ index.php
@@ -11,12 +11,13 @@ include __DIR__ . '/tpl/body_start.php';
 echo"</br><h2>Welcome to Online Memory!</h2></br>";
 echo"<h5>To start a game you need a Game ID. If the other person you want to play with already has a Game ID, enter this ID so you can play together.</h5></br>";
 ?>
-
-<div id="button" class="btn btn-primary mb-2">Generate an ID</div>
+<form action="../webpro/scripts/create_game.php" method="post">
+<button id="button_id" name="button_id" class="btn btn-primary mb-2">Generate an ID</button>
+</form>
 <p>Your Game ID: </p>
 <p id="id"></p>
 
-<form method="POST" action="http://siegfried.webhosting.rug.nl/~s3782808/webpro/game.php" id="welcome_form">
+<form method="POST" id="welcome_form">
     <div class="form-group row">
         <label for="name" class="col-sm-2 col-form-label">Name</label>
         <div class="col-sm-10">
@@ -33,24 +34,8 @@ echo"<h5>To start a game you need a Game ID. If the other person you want to pla
 </form>
 
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    // Create new User Array
-    $new_user = [
-        'name' => $_POST
-        ['name'],
-        'ID' => $_POST
-        ['ID']
-    ];
-    $myJSON = json_encode($new_user);
-    $old_json =  file_get_contents("players.json");
-    $json_decode = json_decode($old_json, true);
-    array_push($json_decode, $new_user);
-    $json_file = fopen('players.json', 'w');
-    fwrite($json_file, json_encode($json_decode));
-    fclose($json_file);
-}
+
+
 
 ?>
 <?php
---
+++ scripts/create_game.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frisostolk
+ * Date: 2019-06-23
+ * Time: 20:45
+ */

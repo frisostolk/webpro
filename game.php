95fafaa94f7e921dd9f5746830df0dc4c4e3e625
frisostolk <frisostolk98@gmail.com>
2019-06-25 12:24:53 +0200

    game

--- game.php
+++ game.php
@@ -3,10 +3,18 @@
 </div>
 <?php
 include __DIR__ . '/tpl/head.php';
+session_start();
 include __DIR__ . '/tpl/body_start.php';
 ?>
 <script src="scripts/game.js"></script>
 <script src="scripts/timer.js"></script>
+<style>
+    #gameid{
+        position: absolute;
+        left: 0px;
+        right: 500px;
+    }
+</style>
 
 <div id="game">
     <div id="timer">You still have <span id="time">10:00</span> minutes/seconds!</div>
@@ -14,6 +22,10 @@ include __DIR__ . '/tpl/body_start.php';
         <p>Scores:</p>
         <p>Player 1:</p>
         <p>Player 2:</p>
+        <?php
+        $hallo = $_SESSION['gameid'];
+        echo $hallo;
+        ?>
     </div>
     <div id="endGameModal" class="modal">
         <div class="modal-content">

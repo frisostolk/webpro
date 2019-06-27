3240cee00570ed319e403a2d212f81ea7d5f280d
frankafranka <f.korpel@student.rug.nl>
2019-06-27 13:59:17 +0200

    added allmatchcheck and sessiondestroy

--- scripts/add_match.php
+++ scripts/add_match.php
@@ -9,4 +9,10 @@ ini_set('display_errors', 1);
     $content['match'] = $match;
     $json_file = fopen('../data/'.$_SESSION['gameid'].'.json', 'w');
     fwrite($json_file, json_encode($content));
+// if the match array contains twelve items all cards have been matched so the endGame modal must pop up end the
+// session has to be destroyed, and maybe delete json file?
+//if (count($content['match']) == 1){
+//    echo '<script src="../scripts/allMatchCheck.js"></script>';
+//    session_destroy();
+//}
 ?>

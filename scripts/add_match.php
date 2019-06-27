2d249e69b2eb3e2425090a7dbbaec3781d3aa6fc
frisostolk <frisostolk98@gmail.com>
2019-06-27 17:45:15 +0200

    teruggehaald

--- scripts/add_match.php
+++ scripts/add_match.php
@@ -6,14 +6,14 @@ ini_set('display_errors', 1);
     $content = json_decode($file, true);
     $match = $content['match'];
     $match[] = $_POST['index'];
+    if($content['sessionID0'] === session_id()){
+        $content['score1'] += 1;
+    }else{
+        $content['score2'] += 1;
+    }
     $content['match'] = $match;
     $json_file = fopen('../data/'.$_SESSION['gameid'].'.json', 'w');
     fwrite($json_file, json_encode($content));
-// if the match array contains twelve items all cards have been matched so the endGame modal must pop up end the
-// session has to be destroyed, and maybe delete json file?
-if (count($content['match']) == 1){
-    echo '<script src="../scripts/all_match_check.js"></script>';
-    session_destroy();
-    echo'het werkt';
-}
+    header('Content-Type: application/json');
+    echo json_encode($content);
 ?>

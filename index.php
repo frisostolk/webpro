9a8efaf9d2da2e5a5792acab83164051bb35429c
frankafranka <f.korpel@student.rug.nl>
2019-06-27 14:33:23 +0200

    validation works now

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ echo"<h5>To start a game you need a Game ID. If the other person you want to pla
             <div class="form-group row">
                 <label for="name_id" class="col-sm-2 col-form-label">Name</label>
                 <div class="col-sm-10">
-                    <input type="text" onkeyup="check_name()" class="form-control" name="name_id" id="name_id" placeholder="Enter your Name">
+                    <input type="text" onkeyup="check_name_id()" class="form-control" name="name_id" id="name_id" placeholder="Enter your Name">
                 </div>
                 <br>
                 <br>
